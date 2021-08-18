abf91968e7206578ccb3e043e100617e1695f319
Júlia Reis <69217117+juliaR29@users.noreply.github.com>
2021-08-18 14:41:22 -0300

    Primeira versão do sistema em java

---
+++ Implementação/src/src/business/Curso.java
@@ -0,0 +1,19 @@
+package business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Curso {
+	
+	private String nome;
+	private int numCreditos;
+	private List<Disciplinas> listaDisciplinas = new ArrayList<Disciplinas>() ;
+	
+	public String getNome() {
+		return nome;
+	}
+	public int getNumCreditos() {
+		return numCreditos;
+	}
+	
+}
