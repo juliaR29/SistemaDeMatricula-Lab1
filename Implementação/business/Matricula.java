591ba1e99ccab86380ae1d8df2218031ce3a6264
Júlia Reis <69217117+juliaR29@users.noreply.github.com>
2021-08-18 14:45:34 -0300

    Add files via upload

---
+++ Implementação/business/Matricula.java
@@ -0,0 +1,23 @@
+package business;
+
+public class Matricula {
+	
+	private boolean matriculaAberta;
+	
+	public boolean isMatriculaAberta() {
+		return matriculaAberta;
+	}
+	public void setMatriculaAberta(boolean matriculaAberta) {
+		this.matriculaAberta = matriculaAberta;
+	}
+	
+	
+	public void efetuarMatricula() {
+		
+	}
+	
+	public void cancelarMatricula() {
+		
+	}
+	
+}
---
+++ Implementação/business/Professor.java
@@ -0,0 +1,28 @@
+package business;
+
+public class Professor {
+	
+	private String nome;
+	private String login;
+	private String senha;
+	private int identificadorProf;
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public int getIdentificadorProf() {
+		return identificadorProf;
+	}
+	
+
+	public void verificarAlunosMatriculados() {
+		
+	}
+
+}
